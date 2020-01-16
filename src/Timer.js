f09259b8e735c3a0af17c51323fe106930db06cd
github username <github email address>
2020-01-16 14:04:51 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -6,7 +6,13 @@ class Timer extends Component {
     color: "#" + Math.floor(Math.random() * 16777215).toString(16)
   };
 
-  // add your code here
+  componentDidMount() {
+    this.interval = setInterval(() => { this.setState({ time:this.state.time+1 }) },1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
 
   render() {
     const { time, color } = this.state;
